3144f14b00851ec90da9f9b2b57f2cc703513e4c
“Aldo <aldo.bushaj@edu.unito.it>
2022-10-15 15:59:12 +0200

    Aggiunta bottoni nel file java

--- BnextAPP/app/src/main/java/com/example/bnext/PaymentActivity.java
+++ BnextAPP/app/src/main/java/com/example/bnext/PaymentActivity.java
@@ -3,12 +3,24 @@ package com.example.bnext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PaymentActivity extends AppCompatActivity {
+
+    TextView totalText;
+
+    Button endTripButton, feedbackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+
+        totalText = findViewById(R.id.totalText);
+        endTripButton = findViewById(R.id.endTripButton);
+        feedbackButton = findViewById(R.id.feedbackButton);
+
     }
 }
