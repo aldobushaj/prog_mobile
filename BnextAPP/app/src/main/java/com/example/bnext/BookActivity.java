6afdf05237a8f34aa5021def8fc3718de3c4b675
“Aldo <aldo.bushaj@edu.unito.it>
2022-10-15 15:53:13 +0200

    Aggiunta book & payment activity

---
+++ BnextAPP/app/src/main/java/com/example/bnext/BookActivity.java
@@ -0,0 +1,29 @@
+package com.example.bnext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class BookActivity extends AppCompatActivity {
+
+    TextView infoText, oreText, priceText;
+    Button startBookButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+
+        infoText = findViewById(R.id.totalText);
+        oreText = findViewById(R.id.oreText);
+        priceText = findViewById(R.id.priceText);
+        startBookButton = findViewById(R.id.feedbackButton);
+
+        priceText.setText("Price / Hour: 12........");
+
+    }
+}
