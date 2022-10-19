774525d1ec1270de57e15a5b1e00c13dfa79798f
Silvestro Frisullo <silvestrofrisullo@gmail.com>
2022-10-19 14:59:52 +0200

    Creata user details activity e aggiunti i collegamenti per andare dalla home a questa e viceversa

---
+++ BnextAPP/app/src/main/java/com/example/bnext/UserDetailsActivity.java
@@ -0,0 +1,42 @@
+package com.example.bnext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class UserDetailsActivity extends AppCompatActivity {
+    String token = "";
+    Button HomeUserDetailsButton, BookRideButton;
+    TextView UserNameTextView;
+    EditText UsernameEditText,EmailEditText;
+    ListView AvailableCarsListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+
+        // ---- assign values to each control of the layout----
+        //Buttons
+        HomeUserDetailsButton = findViewById(R.id.homeUserDetailsButton);
+        // Edit Text
+        UsernameEditText = findViewById(R.id.usernameEditText);
+        EmailEditText = findViewById(R.id.emailEditText);
+
+        HomeUserDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Così ritorniamo alla home quando clicco sul bottone
+                Intent intent = new Intent(view.getContext(), BookRide.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+    }
+}
